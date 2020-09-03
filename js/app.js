5ef6b149f618afd0785bb5712b448e271d6a4e0c
Emeka Allison <emekaallison4@gmail.com>
2020-09-03 11:25:22 +0100

    Updated to allow user input

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const targetDay = "5 Oct 2020"
+let targetDay = ""
 
 
 const daysVal = document.getElementById('days')
@@ -6,24 +6,48 @@ const hoursVal = document.getElementById('hours')
 const minutesVal = document.getElementById('minutes')
 const secondsVal = document.getElementById('seconds')
 
-
-const countDown = () =>{
-    const dDay = new Date(targetDay);
-    const currentDate = new Date();
+const message = document.getElementById('message')
+const error = document.getElementById('error')
 
 
-    const diffSeconds = (dDay - currentDate) / 1000;
+const countDown = tDay =>{
+    try{
+        const dDay = new Date(tDay);
+        const currentDate = new Date();
     
-    const seconds = Math.floor(diffSeconds) % 60;
-    const minutes = Math.floor(diffSeconds/60) % 60;
-    const hours = Math.floor(diffSeconds/3600) % 24;
-    const days = Math.floor(diffSeconds/ 3600/ 24);
+    
+        const diffSeconds = (dDay - currentDate) / 1000;
+
+        if (diffSeconds < 0){
+            error.innerHTML = "You have to enter a future date" 
+
+            daysVal.innerHTML = "-";
+            hoursVal.innerHTML = "--";
+            minutesVal.innerHTML = "--";
+            secondsVal.innerHTML = "--";
+        } 
 
 
-    daysVal.innerHTML = days;
-    hoursVal.innerHTML = formatTime(hours);
-    minutesVal.innerHTML = formatTime(minutes);
-    secondsVal.innerHTML =formatTime(seconds);
+        else{
+            const seconds = Math.floor(diffSeconds) % 60;
+            const minutes = Math.floor(diffSeconds/60) % 60;
+            const hours = Math.floor(diffSeconds/3600) % 24;
+            const days = Math.floor(diffSeconds/ 3600/ 24);
+
+            message.innerHTML = `You have ${days} days until ${targetDay}`
+            error.innerHTML = ""
+        
+        
+            daysVal.innerHTML = days;
+            hoursVal.innerHTML = formatTime(hours);
+            minutesVal.innerHTML = formatTime(minutes);
+            secondsVal.innerHTML = formatTime(seconds);
+        }
+    }
+
+    catch{
+        error.innerHTML = "Please enter time like 1 Jan 2021 not 1st Jan  2021";
+    }
 
 }
 
@@ -33,4 +57,19 @@ function formatTime(time){
 }
 
 
-setInterval(countDown, 1000);
+// 
+
+const captureTime = () =>{
+    const date = document.getElementById("date").value;
+    // console.log(date);
+    return date;
+}
+
+
+document.getElementById('form-time').addEventListener('submit', 
+    (e)=>{
+        e.preventDefault();
+        targetDay = captureTime();
+        setInterval(()=>countDown(targetDay), 1000);
+    }
+)
